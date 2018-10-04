37d734fa692b07b3c72087eb1afdfffe32b05363
Katsunori Tanaka <tanaka-katsunori@dmm.com>
2018-10-04 18:25:48 +0900

    Fix truffle.js

--- truffle.js
+++ truffle.js
@@ -1,10 +1,10 @@
 /*
- * NB: since truffle-hdwallet-provider 0.0.5 you must wrap HDWallet providers in a 
+ * NB: since truffle-hdwallet-provider 0.0.5 you must wrap HDWallet providers in a
  * function when declaring them. Failure to do so will cause commands to hang. ex:
  * ```
  * mainnet: {
- *     provider: function() { 
- *       return new HDWalletProvider(mnemonic, 'https://mainnet.infura.io/<infura-key>') 
+ *     provider: function() {
+ *       return new HDWalletProvider(mnemonic, 'https://mainnet.infura.io/<infura-key>')
  *     },
  *     network_id: '1',
  *     gas: 4500000,
@@ -18,12 +18,6 @@ module.exports = {
     networks: {
         development: {
             host: "127.0.0.1",
-            port: 8545,
-            network_id: "15",
-            gas: 3012388
-        },
-        privatenet: {
-            host: "host.docker.internal",
             port: 8545,
             network_id: "15",
             gas: 3012388
